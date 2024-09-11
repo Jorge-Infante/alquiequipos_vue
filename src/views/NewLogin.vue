929b3ba8b85efb3d67c1985eef5f2dc53300a0d0
Etian Augusto Gutierrez Quintero <123342413+3714n@users.noreply.github.com>
2024-09-11 01:52:58 -0500

    Login con Bootstrap y se eliminaron los componestes antiguos

--- src/views/NewLogin.vue
+++ src/views/NewLogin.vue
@@ -1,18 +1,18 @@
 <template>
-  <HelloWorld />
+  <NuevoLogin />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import NuevoLogin from '../components/NuevoLogin.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    NuevoLogin,
   },
 });
 </script>
