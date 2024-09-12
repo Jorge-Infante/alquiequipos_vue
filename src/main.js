4360aa18db80294aa97d8ffa6fc140565c7e6975
Etian Augusto Gutierrez Quintero <123342413+3714n@users.noreply.github.com>
2024-09-12 02:12:30 -0500

    configuracion de style y separacion de componentes del Login

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+// Instalador (npm install bootstrap @popperjs/core)
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
